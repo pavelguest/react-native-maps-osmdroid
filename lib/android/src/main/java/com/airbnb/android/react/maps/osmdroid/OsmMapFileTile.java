4337fb13a6e95efae2d36b52bcc41d220f21a3c6
Bartłomiej Hołota <bholota@pgs-soft.com>
2019-08-09 15:31:15 +0200

    Fix now multiple tile set archives may be used

--- lib/android/src/main/java/com/airbnb/android/react/maps/osmdroid/OsmMapFileTile.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/osmdroid/OsmMapFileTile.java
@@ -14,6 +14,8 @@ import org.osmdroid.tileprovider.util.SimpleRegisterReceiver;
 import org.osmdroid.views.MapView;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class OsmMapFileTile extends OsmMapFeature {
@@ -49,84 +51,76 @@ public class OsmMapFileTile extends OsmMapFeature {
     this.minimumZ = minimumZ;
   }
 
+  @NonNull
+  private File[] findAllSupportedFilesInDirectory(File directory) {
+    List<File> candidates = new ArrayList<>();
+    File[] list = directory.listFiles();
+    for (int i = 0; i < list.length; i++) {
+      if (list[i].isDirectory()) {
+        continue;
+      }
+      String name = list[i].getName().toLowerCase();
+      if (!name.contains(".")) {
+        continue; //skip files without an extension
+      }
+      name = name.substring(name.lastIndexOf(".") + 1);
+      if (name.length() == 0) {
+        continue;
+      }
+      if (ArchiveFileFactory.isFileExtensionRegistered(name)) {
+        candidates.add(list[i]);
+      }
+    }
+    return candidates.toArray(new File[0]);
+  }
+
   private void setupMapProvider(@NonNull MapView map) {
     //first we'll look at the default location for tiles that we support
     Context context = map.getContext();
-    File f = new File(context.getFilesDir() + "/offline_tiles/");
-    if (f.exists()) {
+    File f = new File(context.getFilesDir() + "/offline_tiles/"); // todo allow parametrization
+    if (f.exists() && f.isDirectory()) {
 
-      File[] list = f.listFiles();
-      if (list != null) {
-        for (int i = 0; i < list.length; i++) {
-          if (list[i].isDirectory()) {
-            continue;
-          }
-          String name = list[i].getName().toLowerCase();
-          if (!name.contains(".")) {
-            continue; //skip files without an extension
-          }
-          name = name.substring(name.lastIndexOf(".") + 1);
-          if (name.length() == 0) {
-            continue;
-          }
-          if (ArchiveFileFactory.isFileExtensionRegistered(name)) {
-            try {
-
-              //ok found a file we support and have a driver for the format, for this demo, we'll
-              // just use the first one
-
-              //create the offline tile provider, it will only do offline file archives
-              //again using the first file
-              OfflineTileProvider tileProvider =
-                  new OfflineTileProvider(new SimpleRegisterReceiver(map.getContext()),
-                      new File[]{list[i]});
-
-              //tell osmdroid to use that provider instead of the default rig which is (asserts,
-              // cache, files/archives, online
-              map.setTileProvider(tileProvider);
-
-              //this bit enables us to find out what tiles sources are available. note, that this
-              // action may take some time to run
-              //and should be ran asynchronously. we've put it inline for simplicity
-
-              String source = "";
-              IArchiveFile[] archives = tileProvider.getArchives();
-              if (archives.length > 0) {
-                //cheating a bit here, get the first archive file and ask for the tile sources
-                // names it contains
-                Set<String> tileSources = archives[0].getTileSources();
-                //presumably, this would be a great place to tell your users which tiles sources
-                // are available
-                if (!tileSources.isEmpty()) {
-                  //ok good, we found at least one tile source, create a basic file based tile
-                  // source using that name
-                  //and set it. If we don't set it, osmdroid will attempt to use the default
-                  // source, which is "MAPNIK",
-                  //which probably won't match your offline tile source, unless it's MAPNIK
-                  source = tileSources.iterator().next();
-                  map.setTileSource(FileBasedTileSource.getSource(source));
-                } else {
-                  map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-                }
-
-              } else {
-                map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-              }
-
-              Toast.makeText(map.getContext(), "Using " + list[i].getAbsolutePath() + " " +
-              source,
-                  Toast.LENGTH_LONG).show();
-              map.invalidate();
-              return;
-            } catch (Exception ex) {
-              ex.printStackTrace();
+      File[] list = findAllSupportedFilesInDirectory(f);
+      if (list.length > 0) {
+        try {
+          OfflineTileProvider tileProvider =
+              new OfflineTileProvider(new SimpleRegisterReceiver(map.getContext()), list);
+          map.setTileProvider(tileProvider);
+          // setup tile source
+          String source = "";
+          IArchiveFile[] archives = tileProvider.getArchives();
+          if (archives.length > 0) {
+            //cheating a bit here, get the first archive file and ask for the tile sources
+            // names it contains
+            Set<String> tileSources = archives[0].getTileSources();
+            //presumably, this would be a great place to tell your users which tiles sources
+            // are available
+            if (!tileSources.isEmpty()) {
+              //ok good, we found at least one tile source, create a basic file based tile
+              // source using that name
+              //and set it. If we don't set it, osmdroid will attempt to use the default
+              // source, which is "MAPNIK",
+              //which probably won't match your offline tile source, unless it's MAPNIK
+              source = tileSources.iterator().next();
+              map.setTileSource(FileBasedTileSource.getSource(source));
+            } else {
+              map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
             }
+
+          } else {
+            map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
           }
+
+          Toast.makeText(map.getContext(), "Using " + source, Toast.LENGTH_LONG).show();
+          map.invalidate();
+          return;
+        } catch (Exception ex) {
+          ex.printStackTrace();
         }
+      } else {
+        Toast.makeText(map.getContext(), f.getAbsolutePath() + " dir not found!", Toast.LENGTH_LONG).show();
       }
       Toast.makeText(map.getContext(), f.getAbsolutePath() + " did not have any files I can open! Try using MOBAC", Toast.LENGTH_LONG).show();
-    } else {
-      Toast.makeText(map.getContext(), f.getAbsolutePath() + " dir not found!", Toast.LENGTH_LONG).show();
     }
   }
 }
